0f78bb643f92e6a63ac3cb1d12c85b13375ff48f
bb2j3z <brian@visitthelab.com>
2016-06-24 10:55:49 -0400

    Initial Population

---
+++ ExportMedia.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ExportMedia.aspx.cs" Inherits="ExportMedia" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <style type="text/css">
+        .input_text {
+            width: 350px;
+        }
+
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+
+            <asp:TextBox ID="txtFolder" Text="/CSI/Images" runat="server" CssClass="input_text"/><br />
+            <br />
+            <asp:Button ID="myButton" Text="Export Images" OnClick="ExportMediaAction" runat="server" />
+
+
+            <br /><br />
+            <a href="ExportMedia.aspx">Restart</a>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ExportMedia.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+
+public partial class ExportMedia : System.Web.UI.Page
+{
+
+    public void ExportMediaAction(object sender, EventArgs e)
+    {
+        var database = Database.GetDatabase("master");
+        var folder = txtFolder.Text;
+
+        var images = database.SelectItems("/sitecore/media library/" + folder  + "/ descendant::*[@@templatekey='jpeg']");
+        String new_file = "";
+        var this_path = Server.MapPath("/") + "/export/";
+
+        int counter = 0;
+        foreach (var image in images)
+        {
+
+            var mediaPath = image.Paths.Path;
+            var mediaItem = (MediaItem)image;
+            var media = MediaManager.GetMedia(mediaItem);
+            var stream = media.GetStream();
+
+            // check for folder
+            mediaPath = mediaPath.Replace("/sitecore/media library", "");
+            string fullPath = Path.GetFullPath(mediaPath).TrimEnd(Path.DirectorySeparatorChar);
+            string projectName = Path.GetFileName(fullPath);
+
+            // check and see if last value is the same as name + don't need to create folder.
+            string path = "";
+            if (projectName != image.Name)
+            {
+                path = this_path + mediaPath;
+            }
+            else
+            {   // Real path
+                path = this_path + mediaPath;
+                path = path.Substring(0, path.Length - projectName.Length);
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            new_file = Path.Combine(path, image.Name + ".jpeg");
+
+            using (var targetStream = File.OpenWrite(new_file))
+            {
+                stream.CopyTo(targetStream);
+                targetStream.Flush();
+            }
+
+            Response.Write("IMAGE:" + counter + ":" + new_file + " saved<BR>");
+            counter += 1;
+
+        }
+
+        if (counter == 0)
+        {
+            Response.Write("No records were found.");
+        }
+        else { 
+            Response.Write("DONE");
+        }
+
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+}
